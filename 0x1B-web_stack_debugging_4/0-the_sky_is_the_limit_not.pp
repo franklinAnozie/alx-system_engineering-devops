4abe9c168949d71324bc1db0c7ceb4885ba7171e
franklinAnozie <cfanozie@gmail.com>
2024-04-16 18:34:49 +0100

    attempted more tasks

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,10 @@
+#making our server accept max requests
+
+exec { 'increase ulimit to 4096':
+    command => 'sed -i "s/15/4096" /etc/default/nginx',
+    path    => '/bin:/usr/bin'
+}
+
+exec { 'restart the server':
+    command => 'service nginx restart'
+}
---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,6 @@
+#making user limitless
+
+exec { 'increase access level of user':
+    command => 'sed -i "/ ^holberton/s/ [0-9]$/ 10000" /etc/security/limits.conf',
+    path    => '/bin:/usr/bin'
+}
