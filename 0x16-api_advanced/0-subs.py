893955fdf07e03f9978adc04e9d51fe3fbdf5749
franklinAnozie <cfanozie@gmail.com>
2024-04-17 15:14:40 +0100

    fixed some changes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,22 +6,15 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ gets the number of subscribers """
-    try:
-        URI = f"https://www.reddit.com/r/{subreddit}/about.json"
-        retVal = None
-        header = {"User-Agent": "0x00"}
-        req = requests.get(url=URI, headers=header, allow_redirects=False)
-        req = req.json()
-    except Exception as e:
-        retVal = 0
-    else:
-        for key in req:
-            if key == "data":
-                for mem in req[key]:
-                    if mem == "subscribers":
-                        retVal = req[key][mem]
-    finally:
-        if retVal is None or retVal == 0:
+    URI = f"https://www.reddit.com/r/{subreddit}/about.json"
+    header = {"User-Agent": "0x00"}
+    req = requests.get(url=URI, headers=header, allow_redirects=False)
+    if req.ok:
+        try:
+            retVal = req.json().get("data").get("subscribers")
+        except Exception:
             return 0
         else:
             return retVal
+    else:
+        return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,28 +6,16 @@ import requests
 
 def top_ten(subreddit):
     """ gets the top ten reddits """
-    try:
-        URI = f"https://www.reddit.com/r/{subreddit}/top.json"
-        header = {"User-Agent": "0x00"}
-        count = 10
-        retVal = []
-        req = requests.get(url=URI, headers=header, allow_redirects=False)
-        req = req.json()
-    except Exception as e:
-        retVal = 0
-    else:
-        for key in req:
-            if key == "data":
-                for mem in req[key]:
-                    if mem == "children":
-                        for nk in req[key][mem]:
-                            if count <= 0:
-                                break
-                            retVal.append(nk["data"]["title"])
-                            count -= 1
-    finally:
-        if retVal == 0 or len(retVal) == 0:
+    URI = f"https://www.reddit.com/r/{subreddit}/top.json"
+    header = {"User-Agent": "0x00"}
+    req = requests.get(url=URI, headers=header, allow_redirects=False)
+    if req.ok:
+        try:
+            retVal = req.json().get("data").get("children")
+        except Exception:
             print(None)
         else:
-            for x in retVal:
-                print(x)
+            for index in range(10):
+                print(retVal[index]["data"]["title"])
+    else:
+        print(None)
