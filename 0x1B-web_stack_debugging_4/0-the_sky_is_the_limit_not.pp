29f24b7b26aae66829256626dbab98a95b457bbc
franklinAnozie <cfanozie@gmail.com>
2024-04-22 15:47:17 +0100

    fixed issues

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,11 @@
 #making our server accept max requests
 
 exec { 'increase ulimit to 4096':
-    command => 'sed -i "s/15/4096" /etc/default/nginx',
+    command => 'sed -i "s/15/4096/" /etc/default/nginx',
     path    => '/bin:/usr/bin'
 }
 
 exec { 'restart the server':
-    command => 'service nginx restart'
+    command => 'service nginx restart',
+    path    => '/bin:/usr/bin'
 }
