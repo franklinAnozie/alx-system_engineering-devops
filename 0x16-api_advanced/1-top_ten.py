43dbcc35548b8e239e63cb3c79f539ce39fe092a
franklinAnozie <cfanozie@gmail.com>
2024-04-29 16:29:01 +0100

    fixed 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
-""" Show top ten reddit posts """
-
+"""
+    1-top_ten module
+"""
 import requests
 
 
 def top_ten(subreddit):
-    """ gets the top ten reddits """
-    URI = f"https://www.reddit.com/r/{subreddit}/top.json"
-    header = {"User-Agent": "0x00"}
-    req = requests.get(url=URI, headers=header, allow_redirects=False)
-    if req.ok:
-        try:
-            retVal = req.json().get("data").get("children")
-        except Exception:
-            print(None)
-            return
-        else:
-            if len(retVal) >= 10:
-                for index in range(10):
-                    print(retVal[index]["data"]["title"])
-            else:
-                for index in range(len(retVal)):
-                    print(retVal[index]["data"]["title"])
-    else:
+    """
+        take one arg the subreddit name
+        sends a get req to an api
+        to get the top 10
+        returns value
+    """
+    url = "https://www.reddit.com/r/{}/top.json".format(subreddit)
+    headers = {"User-Agent": "0x00"}
+
+    data = requests.get(url, headers=headers, allow_redirects=False)
+
+    if data.status_code != 200:
         print(None)
         return
+    value = data.json().get("data")
+    if value is None:
+        print(None)
+        return
+    for post in value.get("children")[0:10]:
+        print(post.get("data").get("title"))
